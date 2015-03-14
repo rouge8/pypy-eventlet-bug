5e5ef14befbd3fde5462ba12900ebaabc09a4e80
Andy Freeland <andy.freeland@redjack.com>
2015-03-14 13:34:18 -0400

    Failing code and deps on PyPy 2.5 on OS X

---
+++ make_requests.py
@@ -0,0 +1,13 @@
+import eventlet
+eventlet.monkey_patch()
+
+import os
+import urllib3
+
+TARGET_HOST = os.environ.get('TARGET_HOST', 'http://localhost:9200')
+TARGET_PATH = os.environ.get('TARGET_PATH', '/test')
+
+pool = urllib3.connection_from_url(TARGET_HOST)
+
+for i in range(1000):
+    pool.request('HEAD', TARGET_PATH)
